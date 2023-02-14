28bfbab30c7a6da0d0604010d5c4ef8a38f2e24c
Rafael <rafaelhayashi8@gmail.com>
2023-02-14 17:49:46 -0300

    refatorando

--- lib/src/shared/widgets/user_image_button.dart
+++ lib/src/shared/widgets/user_image_button.dart
@@ -5,10 +5,12 @@ class UserImageButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final colorScheme = theme.colorScheme;
     return GestureDetector(
       child: CircleAvatar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        foregroundColor: Theme.of(context).colorScheme.primary,
+        backgroundColor: colorScheme.primary,
+        foregroundColor: colorScheme.primary,
         child: const Text('A'),
       ),
     );
