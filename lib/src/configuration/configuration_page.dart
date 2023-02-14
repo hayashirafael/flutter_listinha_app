60f588b50a0ab37489f7a4219f62902c42261a8c
Rafael <rafaelhayashi8@gmail.com>
2023-02-14 17:50:15 -0300

    feat: adc radio list

--- lib/src/configuration/configuration_page.dart
+++ lib/src/configuration/configuration_page.dart
@@ -14,7 +14,53 @@ class _ConfigurationPageState extends State<ConfigurationPage> {
       appBar: AppBar(
         title: const Text('configuration'),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 20,
+          right: 20,
+          left: 20,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Configurações',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Tema',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(height: 10),
+            RadioListTile<ThemeMode>(
+              title: const Text('Sistema'),
+              value: ThemeMode.system,
+              groupValue: ThemeMode.light,
+              onChanged: (mode) {},
+            ),
+            RadioListTile<ThemeMode>(
+              title: const Text('Claro'),
+              value: ThemeMode.light,
+              groupValue: ThemeMode.light,
+              onChanged: (mode) {},
+            ),
+            RadioListTile<ThemeMode>(
+              title: const Text('Escuro'),
+              value: ThemeMode.dark,
+              groupValue: ThemeMode.light,
+              onChanged: (mode) {},
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Controle de dados',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(height: 10),
+            OutlinedButton(onPressed: () {}, child: const Text('Apagar cache e reiniciar o app'))
+          ],
+        ),
+      ),
     );
   }
 }
