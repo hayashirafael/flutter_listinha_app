69af137666318062076b2907bed8d55f131586f0
Rafael <rafaelhayashi8@gmail.com>
2023-02-14 17:48:35 -0300

    chore: concluindo a implementacao do package modular para a navegacao

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -1,9 +1,19 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:listinha/src/configuration/configuration_page.dart';
+import 'package:listinha/src/home/home_module.dart';
+import 'package:listinha/src/shared/stores/app_store.dart';
 
 class AppModule extends Module {
   @override
+  // TODO: implement binds
+  List<Bind> get binds => [
+        Bind.singleton((i) => AppStore()),
+      ];
+  // Singleton, criando apenas 1 instancia
+
+  @override
   List<ModularRoute> get routes => [
-        ChildRoute('/', child: (context, args) => const ConfigurationPage()),
+        ModuleRoute('/home', module: HomeModule()),
+        ChildRoute('/config', child: (context, args) => const ConfigurationPage()),
       ];
 }
